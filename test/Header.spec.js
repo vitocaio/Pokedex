3d992d4b3ff2ae26d5533b171396422acdc6898e
VITOR CAIO CAMPOS <vitocaio.campos@gmail.com>
2018-07-29 20:24:30 -0300

    fix: adjust test header - error in template attribute.

--- test/Header.spec.js
+++ test/Header.spec.js
@@ -1,10 +1,9 @@
-import HeaderComponent from '../src/template/Header'
+import * as HeaderComponent from '../src/template/Header'
 const expect = require('chai').expect
-const Header = new HeaderComponent()
 
 describe('Component Header', () => {
   it('should render a header component', () => {
-    expect(Header.template).to.contains(`<header>`
+    expect(HeaderComponent.template).to.contains(`<header>`
   )
   })
 })
